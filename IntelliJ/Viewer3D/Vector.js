b95769b7eb4d259f88558611aca92141499c1ecf
KubbyDev <gbrljorge@gmail.com>
2019-03-26 18:16:47 +0100

    Debut Viewer3D

---
+++ IntelliJ/Viewer3D/Vector.js
@@ -0,0 +1,62 @@
+class Vector {
+
+    constructor(x, y, z) {
+        this.x = x,
+            this.y = y,
+            this.z = z;
+    }
+
+    static get zero() {
+        return new Vector(0,0,0);
+    }
+
+    multiply(mult) {
+        this.x *= mult;
+        this.y *= mult;
+        this.z *= mult;
+    }
+
+    divide(div) {
+        this.x /= div;
+        this.y /= div;
+        this.z /= div;
+    }
+
+    add(other) {
+        this.x += other.x;
+        this.y += other.y;
+        this.z += other.z;
+    }
+
+    substract(other) {
+        this.x -= other.x;
+        this.y -= other.y;
+        this.z -= other.z;
+    }
+
+    dot(other) {
+        return this.x * other.x + this.y * other.y + this.z * other.z;
+    }
+
+    static sqrDistance(a, b) {
+
+        const sqr = x => x*x;
+        return sqr(b.x - a.x) + sqr(b.y - a.y) + sqr(b.z - a.z);
+    }
+
+    static distance(a, b) {
+        return Math.sqrt(this.sqrDistance(a, b));
+    }
+
+    rotate(h, v) {
+
+    }
+
+    toString() {
+        return "x: " + this.x + ", y: " + this.y + ", z: " + this.z;
+    }
+
+    disp() {
+        console.log(this.toString());
+    }
+}
