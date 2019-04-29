6e44fc11a53abf701e5288815edc245a929c55b9
Gabriel <gbrljorge@gmail.com>
2019-04-29 17:41:13 +0200

    Line

---
+++ IntelliJ/LearningCars/Game/Car.js
@@ -0,0 +1,91 @@
+class Car {
+
+    position = [0,0];
+    rotation = 0;
+    width = 5;
+    length = 12;
+    color = "#FF0000";
+    speed = 1;
+    turnRate = 1;
+    alive = true;
+
+    corners = [[0,0],[0,0],[0,0],[0,0]];
+    hitbox = [new Line(), new Line(), new Line(), new Line()];
+    areCornersCorrect = false;
+
+    constructor() {
+    }
+
+    setColor(color) {
+        this.color = color;
+        return this;
+    }
+
+    setPosition(position) {
+        this.position = position;
+        return this;
+    }
+
+    setPosition(x, y) {
+        this.position = [x, y];
+        return this;
+    }
+
+    setRotation(r) {
+        this.rotation = r;
+        return this;
+    }
+
+    getCorners() {
+        if(!this.areCornersCorrect)
+            this.calcCorners();
+
+        return this.corners;
+    }
+
+    calcCorners() {
+        let degR = this.rotation*Math.PI/180;
+
+        this.corners[0] = [this.position[0] + this.length*Math.cos(degR) - this.width*Math.sin(degR),
+            this.position[1] + this.length*Math.sin(degR) + this.width*Math.cos(degR)];
+
+        this.corners[1] = [this.position[0] + this.length*Math.cos(degR) + this.width*Math.sin(degR),
+            this.position[1] + this.length*Math.sin(degR) - this.width*Math.cos(degR)];
+
+        this.corners[2] = [this.position[0] - this.length*Math.cos(degR) + this.width*Math.sin(degR),
+            this.position[1] - this.length*Math.sin(degR) - this.width*Math.cos(degR)];
+
+        this.corners[3] = [this.position[0] - this.length*Math.cos(degR) - this.width*Math.sin(degR),
+            this.position[1] - this.length*Math.sin(degR) + this.width*Math.cos(degR)];
+
+        this.areCornersCorrect = true;
+    }
+
+    moveForward(enginePower) {
+        this.position[0] += Math.cos(this.rotation)*enginePower*this.speed;
+        this.position[1] += Math.sin(this.rotation)*enginePower*this.speed;
+
+        this.areCornersCorrect = false;
+    }
+
+    turn(input) {
+        this.rotation += this.turnRate * input;
+
+        this.areCornersCorrect = false;
+    }
+
+    isColliding(circuit) {
+
+        this.getCorners();
+        this.hitbox[0].setStartEnd(this.corners[0], this.corners[1]);
+        this.hitbox[1].setStartEnd(this.corners[1], this.corners[2]);
+        this.hitbox[2].setStartEnd(this.corners[2], this.corners[3]);
+        this.hitbox[3].setStartEnd(this.corners[3], this.corners[0]);
+
+        for(let line of this.hitbox)
+            if(line.isColliding(circuit))
+                return true;
+
+        return false;
+    }
+}
---
+++ IntelliJ/LearningCars/Game/Line.js
@@ -0,0 +1,63 @@
+class Line {
+
+    start = [];
+    end = [];
+    visible = false;
+    color = "#555555";
+
+    constructor() {
+    }
+
+    setStartEnd(start, end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    isColliding(lines) {
+
+        for(let line of lines)
+            if(this.collidesWith(line))
+                return true;
+
+        return false;
+    }
+
+    collidesWith(line) {
+
+        return this.getCollisionPoint(line) !== null;
+    }
+
+    getCollisionPoint(other) {
+
+        let point; //Le point d'intersection
+
+        let slopeThis = (this.end[1] - this.start[1]) / (this.end[0] - this.start[0]);
+        let slopeOther = (other.end[1] - other.start[1]) / (other.end[0] - other.start[0]);
+
+        //Si les lignes sont paralleles
+        if (slopeThis === slopeOther)
+            return null;
+
+        let interceptThis = this.start[1] - slopeThis * this.start[0];
+        let interceptOther = other.start[1] - slopeOther * other.start[0];
+
+        //Si une des lignes est verticale
+        if (slopeThis === Infinity)
+            point = [this.start[0], slopeOther * this.start[0] + interceptOther];
+        else if (slopeOther === Infinity)
+            point = [other.start[0], slopeThis * other.start[0] + interceptThis];
+        else {
+
+            //Cas normal (lignes non verticales et non paralleles)
+            let x = (interceptOther - interceptThis) / (slopeThis - slopeOther);
+            point = [x, slopeThis * x + interceptThis];
+        }
+
+        //Si le point est bien sur les deux segments
+        if (point[0] > Math.min(this.start[0], this.end[0]) && point[0] < Math.max(this.start[0], this.end[0])
+            && point[1] > Math.min(this.start[1], this.end[1]) && point[1] < Math.max(this.start[1], this.end[1]))
+            return point;
+
+        return null;
+    }
+}
