4a85c16e15542b967d84fb8300b8fed05c010f9c
Gabriel <gbrljorge@gmail.com>
2019-10-15 22:22:44 +0200

    Assembly TP 5 + F9 to debug in C

--- Assembly/Practicals/calculator.asm
+++ Assembly/Practicals/calculator.asm
@@ -12,10 +12,9 @@ Vector_001      dc.l        Main
 
                 org         $500
 
-Main            movea.l     #String1,a0
-                jsr         RemoveSpace
-                jsr         IsCharError
-                jsr         IsMaxError
+Main            movea.l     #String3,a0
+                
+                jsr         NextOp 
 
                 illegal                
 
@@ -63,11 +62,11 @@ IsCharError     move.l     a0,-(a7)
                 adda.l      #1,a0 
                 bra         \loop
 
-                ; Returns true
-\false           andi.b      #%11111011,ccr
+                ; Returns false
+\false          andi.b      #%11111011,ccr
                 bra         \quit
 
-                ; Returns false
+                ; Returns true
 \true           ori.b       #%00000100,ccr
 
 \quit           move.l     (a7)+,a0
@@ -120,7 +119,89 @@ StrLen          move.l      a0,-(a7) ; Save A0 on the stack.
 
 \quit           movea.l     (a7)+,a0 ; Restore AO from the stack.
                 rts
+
+; Returns the 15-bit unsigned integer represented by the string pointed by a0
+; Sets the Z flag to 1 if there is no error, 0 if there is
+Convert         ; Checks if all the characters are digits
+                jsr         IsCharError
+                beq         \quit
+
+                ; Checks if the integer is less than the maximum
+                jsr         IsMaxError
+                beq         \quit
                 
+                jsr         Atoui
+
+                ; Inverts the Z flag
+\quit           eori.b      #%00000100,ccr
+                rts
+
+; Returns the 16-bit unsigned integer represented by the string pointed by a0
+Atoui           movem.l     d1/a0,-(a7)
+                clr.l       d0
+                clr.l       d1           
+
+\loop           move.b      (a0)+,d1
+                beq         \quit       ; If it encounters the \0 char, quits
+            
+                ; If there is a number
+                mulu.w      #10,d0
+                subi.b      #'0',d1
+                add.w       d1,d0
+
+                bra         \loop
+
+\quit           movem.l     (a7)+,d1/a0
+                rts
+
+; Prints a string in the video output. a0 is the string to display
+; d1 is the x position of the first char, d2 is the y position
+Print           movem.l      a0/d0/d1,-(a7)
+                
+                ; Stops if the character is a null terminator
+\loop           tst.b       (a0)
+                beq         \quit
+
+                ; Draws the character
+                move.b      (a0),d0
+                jsr         PrintChar
+                
+                ; Goes to the next character
+                add.b       #1,d1
+                adda.l      #1,a0
+
+                bra         \loop
+
+\quit           movem.l      (a7)+,a0/d0/d1
+                rts
+
+; Prints a char in the video output. d0 is the char to display
+; d1 is the x position of the char, d2 is the y position
+PrintChar       incbin      "../Given/PrintChar.bin"
+
+; Returns (in a0) the position of the first operator or null terminator
+NextOp          
+\loop           tst.b       (a0)
+                beq         \quit
+
+                cmp.b       #'+',(a0)
+                beq         \quit
+                
+                cmp.b       #'-',(a0)
+                beq         \quit
+                
+                cmp.b       #'*',(a0)
+                beq         \quit
+                
+                cmp.b       #'/',(a0)
+                beq         \quit
+                
+                ; Next char
+                adda.l      #1,a0
+                bra         \loop
+
+\quit           rts
+               
                 ; ==============================
                 ; Data
                 ; ==============================
@@ -128,3 +209,5 @@ StrLen          move.l      a0,-(a7) ; Save A0 on the stack.
                 org         $700
 
 String1         dc.b        "32767",0
+String2         dc.b        "Ceci est un message",0
+String3         dc.b        "43+59",0
--- Linux/Config/.vimrc
+++ Linux/Config/.vimrc
@@ -6,15 +6,17 @@
 " this command: chmod +x run/compile.sh
 
 " F4: Compile
-map <F4> <ESC> :w <CR> :call Compile() <CR>
-:function! Compile()
+map <F4> <ESC> :w <CR> :call Compile(
+:function! Compile(...)
+    " Compilation arguments ("" if nothing given)
+:   let arguments = a:0 >= 1 ? a:1 : ""
     " If compile.sh exists, executes it
 :   if(filereadable("compile.sh"))
-:       !./compile.sh && echo "Compilation successfull"
+:       execute '!./compile.sh ' arguments ' && echo "Compilation successfull"'
     " else run the standard compilation command
     " C
 :   elseif(&filetype == "c" || &filetype == "h")
-:       !gcc -Wall -Wextra -Werror -std=c99 -O1 -o a.out *.[co] && echo "Compilation successfull"
+:       execute '!gcc -Wall -Wextra -Werror -std=c99 -O1 -o a.out *.[co] ' arguments ' && echo "Compilation successfull"'
     " Assembly
 :   elseif(&filetype == "asm")
 :       !/home/kubby/68000/a68k % -o%:r.hex -s -n -rmal && echo "Compilation successfull"
@@ -22,16 +24,18 @@ map <F4> <ESC> :w <CR> :call Compile() <CR>
 :endfunction
 
 " F5: Run
-map <F5> <ESC> :call Run() <CR>
-:function! Run()
+map <F5> <ESC> :call Run(
+:function! Run(...)
+    " Arguments for the execution ("" if nothing given)
+:   let arguments = a:0 >= 1 ? a:1 : ""
     " If run.sh exists, executes it
 :   if(filereadable("run.sh"))
-:       !./run.sh
+:       execute '!./run.sh ' arguments
     " else run the standard run command
     " C
 :   elseif((&filetype == "c" || &filetype == "h"))
 :       if(filereadable("a.out"))
-:           !./a.out && rm a.out 
+:           execute '!./a.out ' arguments ' & rm a.out'
 :       else
 :           !echo "Compiled file not found"
 :       endif
@@ -52,6 +56,24 @@ map <F8> <ESC> :w <CR> :call CompileRun() <CR>
 :   call Run()
 :endfunction
 
+" F9: Debug
+map <F9> <ESC> :w <CR> :call Debug(
+:function! Debug(...)
+    " Arguments for the execution ("" if nothing given)
+:   let runArgs = a:0 >= 1 ? a:1 : ""
+    " Compilation arguments ("" if nothing given)
+:   let compArgs = a:0 >= 2 ? a:2 : ""
+    " C
+:   if(&filetype == "c" || &filetype == "h")
+:       call Compile('-g '.compArgs)
+:       execute '!gdb --args ./a.out ' runArgs ' && rm ./a.out'
+    " Assembly
+:   elseif(&filetype == "asm")
+:       call Compile()
+:       call Run()
+:   endif
+:endfunction
+
 " ----------------------------------------------------------
 " Plugins
 " ----------------------------------------------------------
@@ -66,6 +88,7 @@ Plugin 'VundleVim/Vundle.vim'
 Plugin 'joshdick/onedark.vim'
 Plugin 'SirVer/ultisnips'
 Plugin 'honza/vim-snippets'
+Plugin 'justinmk/vim-syntax-extra'
 
 call vundle#end()
 filetype plugin indent on
