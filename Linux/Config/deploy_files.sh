ea9b832a61679722d55b8810bccd4522713aaaf4
KubbyDev <gbrljorge@gmail.com>
2021-01-25 22:21:31 +0100

    better deploy_files

--- Linux/Config/deploy_files.sh
+++ Linux/Config/deploy_files.sh
@@ -1,11 +1,11 @@
 cd ~/Code/Linux/Config
 
-# Puts the current files in the trash to avoid
+# Puts the current files in tmp to avoid
 # deleting them by accident
-trash ~/.vimrc
-trash ~/.bash_aliases
-trash ~/.bashrc
-trash ~/.zshrc
+mv ~/.vimrc /tmp
+mv ~/.bash_aliases /tmp
+mv ~/.bashrc /tmp
+mv ~/.zshrc /tmp
 
 dos2unix .vimrc
 dos2unix .zshrc
